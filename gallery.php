547180c1a973003761679fb483085f06b8a04ecd
Mark Nichols <mark@zanshin.net>
2012-01-31 22:03:25 -0600

    new javascript enabled Flickr set slideshow

---
+++ gallery.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC
+	"-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link href='http://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
+	<title>Elfenbein Klaviermusik - Piano Instruction for the Dedicated Student</title>
+	<meta name="keywords" content="piano, piano teaching, piano instruction, private piano instruction, piano performance, Manhattan, Junction City, Wamego, Riley, Ogden" />
+	<meta name="description" content="Private piano studio in Manhattan Kansas for dedicated students of all ages." />
+	<meta name="author" content="sibylle kuder" />
+	<meta name="copyright" content="Copyright 2007-2009 Sibylle Kuder" />
+	<meta http-equiv="imagetoolbar" content="no" />
+	<meta name="revisit-after" content="3 days" />
+	<meta name="robots" content="all" />
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/ico" />
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	<link rel="stylesheet" type="text/css" href="thickbox.css" /> 
+<!-- >
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+	<script type="text/javascript" src="galleria/galleria-1.2.6.min.js"></script>
+	<script type="text/javascript" src="galleria/plugins/flickr/galleria.flickr.min.js"></script>
+	<script type="text/javascript" src="galleria/themes/classic/galleria.classic.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script> -->
+	
+	<!-- http://www.flickrshow.co.uk/ -->
+	<script src="http://www.flickrshow.co.uk/static/scripts/flickrshow-7.2.min.js"></script>
+	<script>
+	    var cesc = new flickrshow('cesc', {
+			autoplay:true,
+			interval:5000,
+	        set:'72157614491723890'
+	    });
+	</script>
+	
+	
+	<script src="/mint/?js" type="text/javascript"></script>
+
+</head>
+
+<body>
+    <div id="container">
+		<div id="header">
+			<?php include("header.php")?>
+		</div>
+
+		<div id="navcontainer">
+			<?php include("navigation.php")?>
+		</div>
+		<div id="content">
+			<h1>Photographs from the Studio, travels, and elsewhere.</h1>
+      <p>
+        The following slide show requires JavaScript be enabled in your browser. By hovering your mouse
+        over the image you can see the image caption, a play/stop control, and controls to move forward
+        or backward through the images.
+		
+			<div style="width:100%; height:100%;" id="cesc">
+			    <p>Please enable Javascript to view this slideshow</p>
+			<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+			</div>
+
+			<br /><br />
+			<div id="navfooter">
+				<?php include("navigation.php")?>
+			</div>
+        </div>
+		<div id="footer">
+			<?php include("footer.php")?>
+		</div>
+		</div>
+		<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+		</script>
+		<script type="text/javascript">
+			_uacct = "UA-373283-1";
+
+
+			urchinTracker();
+		</script>
+    </div>
+</body>
