c7cedfc8527a7d9329a840459f366f012d8a6a70
Mark Nichols <mark@zanshin.net>
2012-05-19 21:43:30 -0500

    adding second announcement include. one is nice, two is nicer

---
+++ announcement2.php
@@ -0,0 +1,3 @@
+    <p style="border: 1px solid; background-color: #fcefa7; padding: 5px; text-align: center;">
+      &#9834; Summer Lessons Start June 4th! <br />See the <a href="http://sibyllekuder.com/tuition.php" title"Tuition">Tuition</a> page for more details.  &#9835;
+    </p>
